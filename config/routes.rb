7cd6fcb489fce8a7fd09efc2cf5d06963b833495
Aslıhan Nurcan Çetin <aslihan.cetin@n2mobil.com.tr>
2021-05-30 01:02:03 +0300

    first

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  root "products#index"
+  get "/products", to: "products#index"
+  get "/products/new", to: "products#new"
+  post "/products/create"
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
