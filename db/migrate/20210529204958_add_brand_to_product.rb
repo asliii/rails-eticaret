fbdb755a2341e6f96c133730086470c7c9cd42c0
Aslıhan Nurcan Çetin <aslihan.cetin@n2mobil.com.tr>
2021-05-30 23:35:15 +0300

    fix

--- db/migrate/20210529204958_add_brand_to_product.rb
+++ db/migrate/20210529204958_add_brand_to_product.rb
@@ -1,5 +1,5 @@
 class AddBrandToProduct < ActiveRecord::Migration[5.2]
   def change
-    add_reference :products, :brand, foreign_key: true
+    add_reference :products, :brand, foreign_key: true, null: false
   end
 end
--- db/migrate/20210529205111_add_type_to_product.rb
+++ db/migrate/20210529205111_add_type_to_product.rb
@@ -1,5 +1,5 @@
 class AddTypeToProduct < ActiveRecord::Migration[5.2]
   def change
-    add_reference :products, :type, foreign_key: true
+    add_reference :products, :type, foreign_key: true, null: false
   end
 end
