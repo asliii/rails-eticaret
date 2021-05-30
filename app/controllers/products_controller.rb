87f077abce072a3514c5f6b8f596b2fe5735540a
Aslıhan Nurcan Çetin <aslihan.cetin@n2mobil.com.tr>
2021-05-30 23:33:40 +0300

    fix

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @products = Product.joins(:brand)
   end
 
   def show
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ApplicationRecord
-    has_many :brand, class_name: "Brand", foreign_key: "brand_id"
-    has_many :type, class_name: "Type", foreign_key: "type_id"
+    belongs_to :brand
+    belongs_to :type
 end
